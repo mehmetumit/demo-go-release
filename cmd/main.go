470a33aa9e69f40702bb9ddfaffe65fb64098ff0
Mehmet Ümit Özden <ozdenmehmetumit@gmail.com>
2023-09-30 15:14:48 +0300

    Version generation using ldflags

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"demo/pkg/version"
+	"fmt"
+)
 
 func main(){
+    fmt.Printf("Version: %s \nCommit: %s\n", version.Version, version.GitCommit)
     fmt.Println("Demo")
-
 }
